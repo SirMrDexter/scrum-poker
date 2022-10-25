5d80f4f6b9b85fedeb19f43a0fd6f6c8df72bb76
Xavier Canchal <xaviercanchal@gmail.com>
2022-10-25 11:02:41 +0200

    Add buy me a coffee button in home page

--- pages/home/home.logic.js
+++ pages/home/home.logic.js
@@ -91,6 +91,9 @@ const Home = ({ className }) => {
           >
             Create room
           </Button>
+          <a href="https://www.buymeacoffee.com/xcanchal" style={{ marginTop: '40px' }}>
+            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=xcanchal&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" />
+          </a>
         </div>
       </Layout>
     </div>
